7f3c0891caba855be51a3cd4c6645cb9c6871df6
MartijnEppenga <m.eppenga@forze-delft.nl>
2018-07-06 14:26:05 +0200

    added launcher to run the simulation

---
+++ launcher.py
@@ -0,0 +1,8 @@
+import importlib
+import simulation
+import data_processing
+
+importlib.reload(simulation)
+results = simulation.start()
+importlib.reload(data_processing)
+data_processing.start(*results)
